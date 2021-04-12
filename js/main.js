2b000bec0c7405acc0992fd339033173b2ebc6f8
Svetlana <lampa1979@mail.ru>
2021-04-12 17:10:09 +0400

    fix lesson06

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ let isNumber = function (n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 }
  
-function tryToGame() {
+/* function tryToGame() {
     let input = prompt('Введи число от 1 до 100!');   
     if (isNumber(input)) {   
     input = +input;
@@ -23,10 +23,42 @@ function tryToGame() {
     return alert('Можно вводить только числа');
   }tryToGame();
 }
+tryToGame(); */
+
+
+const tryToGame = function() {
+    let ranNumber = randomNumber;
+    console.log('rNumber: ', ranNumber);
+    const getTryToGame = function () {   
+        const input = prompt('"Угадай число от 1 до 100"');
+        if (input === null) {
+            alert('Игра Окончена!');
+            return;
+        }
+        if (isNumber(input)) {
+            const userNum = +input;
+            if (userNum > randomNumber) {
+                alert('Загаданное число меньше');
+                getTryToGame();
+            } else if (userNum < randomNumber) {
+                alert('Загаданное число больше');
+                getTryToGame();
+            } else {
+                alert('Поздравляю, Вы угадали!!!');
+              return;
+             
+            }
+          } else {
+            alert('Введите число');
+            getTryToGame();
+        }
+    };
+  getTryToGame();
+ 
+};
+
 tryToGame();
 
-
- 
 
  
 
