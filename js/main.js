3648035ddcdebd04c7c6bf3765d1de6b5e285a9c
Svetlana <lampa1979@mail.ru>
2021-04-12 16:09:14 +0400

    Fix main.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,34 @@
 'use strict';
 
+let randomNumber = parseInt(Math.random() * 100);  
+   
+let isNumber = function (n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+ 
+function tryToGame() {
+    let input = prompt('Введи число от 1 до 100!');   
+    if (isNumber(input)) {   
+    input = +input;
+
+      if (input > randomNumber) {
+        alert('Загаданное число меньше ', 'Введи другое число!');
+      } else if (input < randomNumber) {
+        alert('Загаданное число больше ', 'Введи другое число!');
+      } else if (input === randomNumber) {
+        alert('Поздравляю, Вы угадали!!!');
+        return input;
+      }
+   } else {
+    return alert('Можно вводить только числа');
+  }tryToGame();
+}
+tryToGame();
+
+
+ 
+
+ 
+
+
+
